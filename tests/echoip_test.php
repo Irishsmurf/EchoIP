b5ef650ab2e2bf026152101649cee21e6834fc4d
Paddez <paddez@redbrick.dcu.ie>
2016-01-17 10:43:34 +0000

    Seperate tests

--- tests/echoip_test.php
+++ tests/echoip_test.php
@@ -2,12 +2,14 @@
 require_once dirname(__FILE__).'/../index.php';
 
 class EchoIpTest extends PHPUnit_Framework_TestCase {
-  public function testGetIp() {
+  public function testGetIpV4() {
     $this->assertEquals('192.168.0.1', getIP('192.168.0.1', false));
     $this->assertEquals('{"ip":"192.168.0.1"}', getIP('192.168.0.1', true));
+  }
 
+  public function testGetIpV6() {
     $this->assertEquals('2001:0db8:0a0b:12f0:0000:0000:0000:0001', getIP('2001:0db8:0a0b:12f0:0000:0000:0000:0001', false));
-    $this->assertEquals('{"ip":"2001:0db8:0a0b:12f0:0000:0000:0000:0001"}', getIP('2001:0db8:0a0b:12f0:0000:0000:0000:0001', true));
+    $this->assertEquals('{"ip":"2001:0db8:0a0b:12f0:0000:0000:0000:0001"}', getIP('2001:0db8:0a0b:12f0:0000:0000:0000:0001', true));    
   }
 }
 
