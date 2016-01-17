f1ab65b2469e2900101edc3ae1e2d96daa0524fe
Paddez <paddez@redbrick.dcu.ie>
2016-01-17 10:40:12 +0000

    IPv6 testing

--- tests/echoip_test.php
+++ tests/echoip_test.php
@@ -5,6 +5,9 @@ class EchoIpTest extends PHPUnit_Framework_TestCase {
   public function testGetIp() {
     $this->assertEquals('192.168.0.1', getIP('192.168.0.1', false));
     $this->assertEquals('{"ip":"192.168.0.1"}', getIP('192.168.0.1', true));
+
+    $this->assertEquals('2001:0db8:0a0b:12f0:0000:0000:0000:0001', getIP('2001:0db8:0a0b:12f0:0000:0000:0000:0001', false));
+    $this->assertEquals('{"ip":"2001:0db8:0a0b:12f0:0000:0000:0000:0001"}', getIP('2001:0db8:0a0b:12f0:0000:0000:0000:0001', true));
   }
 }
 
